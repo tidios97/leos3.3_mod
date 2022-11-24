b3b5b743347273a4f9a585843af4b336a4d42b7f
adriacr <cristobal.adrian@ext.ec.europa.eu>
2022-11-24 17:45:37 +0100

    CORE#93: DocuWrite bean class renamed.

--- modules/integration/src/main/java/eu/europa/ec/leos/integration/LeosDocuWriteServiceImpl.java
+++ modules/integration/src/main/java/eu/europa/ec/leos/integration/LeosDocuWriteServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 
 @Service
-public class DocuWriteServiceImpl implements DocuWriteService {
+public class LeosDocuWriteServiceImpl implements DocuWriteService {
     @Override
     public byte[] convert(File legFile) throws Exception {
         throw new IllegalStateException("Feature not implemented for the running instance");
